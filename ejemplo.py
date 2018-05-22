b2c1fed5e2ad8a5a0a7cd3061c549d7b71bd177f
Juan M de la Fuente <Jmdelafuente22@users.noreply.github.com>
2018-05-22 16:53:31 -0300

    Add files via upload

--- ejemplo.py
+++ ejemplo.py
@@ -14,19 +14,54 @@ def c(x):
     print x
     
 x=1
+#letras = string.ascii_lowercase
+#letras = string.join(map(chr, range(97, 123)),'\',\'') #or list(map(chr, range(ord('a'), ord('z')+1)))
+letras =  "\'"+(string.join(map(chr, range(97, 123)),'\',\'')).replace("\"","'")+"'"
+estados = {
+    'a': a,
+    'b': b,
+    'c': c,
+    'd': a,
+    'e': a,
+    'f': a,
+    'g': a,
+    'h': a,
+    'i': a,
+    'j': a,
+    'k': a,
+    'l': a,
+    'm': a,
+    'n': a,
+    'o': a,
+    'p': a,
+    'q': a,
+    'r': a,
+    's': a,
+    't': a,
+    'u': a,
+    'v': a,
+    'w': a,
+    'x': a,
+    'y': a,
+    'z': a,
+    '+': a,
+    '-': a,
+    '*': a,
+    '/': a,
+    '=': a,
+    ':=': a,
+}
+print estados
+
 for i in xrange(10):
     state= random.choice('abc')
     print "El estado es: " + state
     print "X es: " + repr(x)
-    result = {
-        'a': a,
-        'b': b,
-        'c': c,
-    }
-    valor = result[state](x)
-    x = x+1
+
+
+    valor = estados[state](x)
+    #x = x+1
 print "Que hermoso switch que hice en Python"
 
 
     
-print repr(result['a'](1))
