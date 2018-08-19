e6bb716dd231cf2c61fb92064d1c3e470f41ffae
Dai Bonet <dainbonet@gmail.com>
2018-08-19 01:11:19 -0300

    Fixed: errores en el parentizado y el not. Cambios en los tokens para identificar AND, OR y NOT.

--- linux/analizadorSintactico.py
+++ linux/analizadorSintactico.py
@@ -160,8 +160,8 @@ def sentenciaOptativa2():
 def no():
 	if(verbose):
 		print("not")
-	if(preanalisis == "operador_logico"):
-		match("operador_logico")
+	if(preanalisis == "not"):
+		match("not")
 		
 
 def sentencia():
@@ -212,8 +212,8 @@ def asignacionollamada():
 def expresionAritmetica():
 	if(verbose):
 		print("expresionAritmetica")
-	case1 = {"write","true","false","read","operador_aritmetico"}
-	if ((preanalisis == "identificador") or (preanalisis == "numero") or (preanalisis in case1)):
+	case1 = {"write","true","false","read","parentesis_a","operador_aritmetico"}
+	if((preanalisis == "identificador") or (preanalisis == "numero") or (preanalisis in case1)):
 		termino() 
 		expresionAritmetica1()
 	else:
@@ -246,36 +246,37 @@ def termino1():
 	if(verbose):
 		print("termino1")
 	if (preanalisis == "operador_termino"):
-				match("operador_termino")
-				factor()
-				termino1()
+		match("operador_termino")
+		factor()
+		termino1()
+		
 def factor():
 	if(verbose):
 		print("factor")
 	if (preanalisis == "identificador"):
-				identificador()
-				factor1()
+		identificador()
+		factor1()
 	elif (preanalisis == "write"):
-				match("write")
-				match("parentesis_a")
-				llamada2()
+		match("write")
+		match("parentesis_a")
+		llamada2()
 	elif (preanalisis == "read"):
-				match("read")
-				match("parentesis_a")
-				identificador()
-				match("parentesis_c")
-	elif (preanalisis == "numero" or preanalisis == "operador_aritmetico") :
-				digitos()
+		match("read")
+		match("parentesis_a")
+		identificador()
+		match("parentesis_c")
+	elif (preanalisis == "numero" or preanalisis == "operador_aritmetico"):
+		digitos()
 	elif (preanalisis == "true"):
-				match("true")
+		match("true")
 	elif (preanalisis == "false"):
-				match("false")
-        elif (preanalisis == "parentesis_a"):
-                                match("parentesis_a")
-                                expresionAritmetica()
-                                match("parentesis_c")
+		match("false")
+	elif (preanalisis == "parentesis_a"):
+		match("parentesis_a")
+		expresionGeneral()
+		match("parentesis_c")
 	else:
-				reportar("Error de sintaxis: se esperaba WRITE,READ,TRUE,FALSE,DIGITO o Identificador valido",preanalisis,"factor")
+		reportar("Error de sintaxis: se esperaba WRITE,READ,TRUE,FALSE,DIGITO,NOT,( o Identificador valido",preanalisis,"factor")
 
 def factor1():
 	if(verbose):
@@ -283,7 +284,7 @@ def factor1():
 	if ( preanalisis == "parentesis_a"):
 		match("parentesis_a")
 		llamada1()
-
+		
 def operadorRelacional():
 	if(verbose):
 		print("operadorRelacional")
@@ -312,14 +313,14 @@ def declaracionVariableOpt():
 	if(verbose):
 		print("declaracionVariablesOpt")
 	if ( preanalisis == "var"):
-			 declaracionVariables()
+		declaracionVariables()
 
 def programaRepPyf():
 	if(verbose):
 		print("programaRepPyf")
 	if (( preanalisis == "function") or (preanalisis == "procedure")):
-			 declaracionPyf()
-			 programaRepPyf()
+		declaracionPyf()
+		programaRepPyf()
 	
 
 def programaRepSentencia():
@@ -328,13 +329,13 @@ def programaRepSentencia():
 	global letras
 	caso1={"begin", "read", "write","while","if"}
 	if ( (preanalisis in caso1) or (preanalisis == "identificador")):
-			 compuesta()
-			 programaRepSentencia()
+		compuesta()
+		programaRepSentencia()
 	
 def expresionGeneral():
 	if(verbose):
 		print("expresionGeneral")
-	caso1={'false', 'true', 'parentesis_a', 'operador_aritmetico', 'write', 'read', 'operador_logico'}
+	caso1={'false', 'true', 'parentesis_a', 'operador_aritmetico', 'write', 'read','not'}
 	if((preanalisis == "identificador") or (preanalisis == "numero") or (preanalisis in caso1)):
 		compararAnd()
 		expresionGeneral1()
@@ -344,23 +345,24 @@ def expresionGeneral():
 def expresionGeneral1():
 	if(verbose):
 		print("expresionGeneral1")
-	if(preanalisis == "operador_logico"):
-		match("operador_logico")
+	if(preanalisis == "or"):
+		match("or")
 		compararAnd()
 		expresionGeneral1()
 
 def compararAnd():
 	if(verbose):
 		print("compararAnd")
-	caso2={'false', 'true', 'operador_aritmetico', 'write', 'read'}
-	if(preanalisis == "operador_logico" or preanalisis== "parentesis_a"):
-		no()
-		match("parentesis_a")
-		expresionGeneral()
-		match("parentesis_c")
-                termino1()
+	caso2={'false', 'true', 'operador_aritmetico', 'write', 'read', 'parentesis_a','not'}
+	if(preanalisis == "and"): #or preanalisis== "parentesis_a"):
+		#no()
+	#	match("parentesis_a")
+	#	expresionGeneral()
+	#	match("parentesis_c")
+     #           termino1()
 		compararAnd1()
 	elif ((preanalisis == "identificador" or preanalisis == "numero" ) or (preanalisis in caso2)):
+		no()
 		expresionAritmetica() 
 		expresionAritmetica2() 
 		compararAnd1()
@@ -377,8 +379,8 @@ def expresionAritmetica2():
 def compararAnd1():
 	if(verbose):
 		print("compararAnd1")
-	if(preanalisis == "operador_logico"):
-		match("operador_logico")
+	if(preanalisis == "and"):
+		match("and")
 		compararAnd()
 
 
@@ -386,78 +388,78 @@ def ifthen():
 	if(verbose):
 		print("ifthen")
 	if ( preanalisis == "if"):
-			 match("if")
-			 expresionGeneral()
-			 match("then")
-			 ifthen1()
+		match("if")
+		expresionGeneral()
+		match("then")
+		ifthen1()
 	else:
-			 reportar("Error de sintaxis: se esperaba IF expresion THEN",preanalisis,"ifthen")
+		reportar("Error de sintaxis: se esperaba IF expresion THEN",preanalisis,"ifthen")
 
 def ifthen1():
 	if(verbose):
 		print("ifthen1")
 	caso2={"read", "write","while","if"}
 	if ( preanalisis == "begin"):
-			 match("begin")
-			 compuesta()
-			 match("end")
-			 alternativa()
+		match("begin")
+		compuesta()
+		match("end")
+		alternativa()
 	elif ((preanalisis in caso2) or (preanalisis == "identificador")):
-			 sentencia()
-			 match("punto_coma")
+		sentencia()
+		match("punto_coma")
 	else: 
-			 reportar("Error de sintaxis: se esperaba WRITE,READ,WHILE,IF,BEGIN o Identificador valido",preanalisis,"ifthen1")
+		reportar("Error de sintaxis: se esperaba WRITE,READ,WHILE,IF,BEGIN o Identificador valido",preanalisis,"ifthen1")
 
 def alternativa():
 	if(verbose):
 		print("alternativa")
 	global letras
 	if  (preanalisis == "punto_coma"):
-			 match("punto_coma")
+		match("punto_coma")
 	elif (preanalisis == "else"):
-			 match("else")
-			 compuesta()
+		match("else")
+		compuesta()
 	else:
-			 reportar("Error de sintaxis: se esperaba ; o ELSE",preanalisis,"alternativa")
+		reportar("Error de sintaxis: se esperaba ; o ELSE",preanalisis,"alternativa")
  
 def mientras():
 	if(verbose):
 		print("mientras")
 	if ( preanalisis == "while"):
-			 match("while")
-			 expresionGeneral()
-			 match("do")
-			 sentenciaCompuesta()
+		match("while")
+		expresionGeneral()
+		match("do")
+		sentenciaCompuesta()
 	else:
-			 reportar("Error de sintaxis: se esperaba WHILE",preanalisis,"mientras")
+		reportar("Error de sintaxis: se esperaba WHILE",preanalisis,"mientras")
 
 def declaracionPyf():
 	if(verbose):
 		print("declaracionPyf")
 	if ( preanalisis == "procedure"):
-			 match("procedure")
-			 identificador()
-			 match("parentesis_a")
-			 parametrosRep()
-			 match("parentesis_c")
-			 match("punto_coma")
-			 declaracionVariableOpt()
-			 declaracionPyfRep()
-			 sentenciaCompuesta()
+		match("procedure")
+		identificador()
+		match("parentesis_a")
+		parametrosRep()
+		match("parentesis_c")
+		match("punto_coma")
+		declaracionVariableOpt()
+		declaracionPyfRep()
+		sentenciaCompuesta()
 	elif ( preanalisis == "function"):
-			 match("function")
-			 identificador()
-			 match("parentesis_a")
-			 parametrosRep()
-			 match("parentesis_c")
-			 match("dos_punto")
-			 tipoVariables()
-			 match("punto_coma")
-			 declaracionVariableOpt()
-			 declaracionPyfRep()
-			 sentenciaCompuesta()
+		match("function")
+		identificador()
+		match("parentesis_a")
+		parametrosRep()
+		match("parentesis_c")
+		match("dos_punto")
+		tipoVariables()
+		match("punto_coma")
+		declaracionVariableOpt()
+		declaracionPyfRep()
+		sentenciaCompuesta()
 	else:
-			 reportar("Error de sintaxis: se esperaba PROCEDURE o FUNCTION",preanalisis,"declaracionPyf")
+		reportar("Error de sintaxis: se esperaba PROCEDURE o FUNCTION",preanalisis,"declaracionPyf")
 
 def parametrosRep():
 	if(verbose):
@@ -471,60 +473,60 @@ def declaracionVariablesRep():
 	if(verbose):
 		print("declaracionVariablesRep")
 	if ( preanalisis == "var"):
-			 declaracionVariables()
+		declaracionVariables()
 
 def declaracionPyfRep():
 	if(verbose):
 		print("declaracionPyfRep")
 	if ( preanalisis == "function" or preanalisis == "procedure"):
-			 declaracionPyf()
-			 declaracionPyfRep()
+		declaracionPyf()
+		declaracionPyfRep()
 	
 	
 def parametrosFormalesRep():
 	if(verbose):
 		print("parametrosFormalesRep")
 	if ( preanalisis == "coma"):
-			 match("coma")
-			 listaVariables()
-			 parametrosFormalesRep()
+		match("coma")
+		listaVariables()
+		parametrosFormalesRep()
 
 def parametrosReales():
 	if(verbose):
 		print("parametrosReales")
 	caso1={'false', 'true', 'parentesis_a', 'operador_aritmetico', 'write', 'read', 'operador_logico'}
 	if(preanalisis == "identificador") or (preanalisis == "numero") or (preanalisis in caso1):
-			 expresionGeneral()
-			 parametrosRealesRep()
+		expresionGeneral()
+		parametrosRealesRep()
 
 def parametrosRealesRep():
 	if(verbose):
 		print("parametrosRealesRep")
 	if ( preanalisis == "coma"):
-			 match("coma")
-			 expresionGeneral()
-			 parametrosRealesRep()
+		match("coma")
+		expresionGeneral()
+		parametrosRealesRep()
 
 def llamadaProcedimiento():
 	if(verbose):
 		print("llamadaProcedimiento")
 	global letras
 	if ( preanalisis == "write"):
-			 match("write")
-			 match("parentesis_a")
-			 llamada2()
-			 #parametrosRealesRep()
+		match("write")
+		match("parentesis_a")
+		llamada2()
+		#parametrosRealesRep()
 	elif ( preanalisis == "read"):
-			 match("read")
-			 match("parentessis_a")
-			 identificador()
-			 match("parentesis_c")
+		match("read")
+		match("parentessis_a")
+		identificador()
+		match("parentesis_c")
 	elif ( preanalisis == "identificador"):
-			 identificador()
-			 match("parentesis_a")
-			 llamada1()
+		identificador()
+		match("parentesis_a")
+		llamada1()
 	else:
-			 reportar("Error de sintaxis: se esparaba llamada a Procedimiento o Funcion",preanalisis,"llamadaProcedimiento")
+		reportar("Error de sintaxis: se esparaba llamada a Procedimiento o Funcion",preanalisis,"llamadaProcedimiento")
 
 def llamada1():
 	if(verbose):
@@ -532,35 +534,35 @@ def llamada1():
 	global letras
 	caso2={'false', 'true', 'parentesis_a', 'operador_aritmetico', 'write', 'read', 'operador_logico'}
 	if ( preanalisis == "parentesis_c"):
-			 match("parentesis_c")
+		match("parentesis_c")
 	elif((preanalisis == "identificador") or (preanalisis == "numero") or preanalisis in caso2):
-			 parametrosReales()
-			 parametrosReales2()
-			 match("parentesis_c")
+		parametrosReales()
+		parametrosReales2()
+		match("parentesis_c")
 	else:
-			 reportar("Error de sintaxis: se esperaba WRITE,READ,NOT,TRUE,FALSE,(,-,Digito o Identificador Valido",preanalisis,"llamada1")
+		reportar("Error de sintaxis: se esperaba WRITE,READ,NOT,TRUE,FALSE,(,-,Digito o Identificador Valido",preanalisis,"llamada1")
 
 def llamada2():
 	if(verbose):
 		print("llamada2")
 	global letras
 	if ( (preanalisis == "identificador")):
-			 identificador()
-			 match("parentesis_c")
+		identificador()
+		match("parentesis_c")
 	elif ( preanalisis == "identificador") or (preanalisis == "operador_aritmetico"):
-			 digitos()
-			 match("parentecis_c")
+		digitos()
+		match("parentecis_c")
 	else:
-			 reportar("Error de sintaxis: se esperaba Digitos o un Identificador Valido",preanalisis,"llamada2")
+		reportar("Error de sintaxis: se esperaba Digitos o un Identificador Valido",preanalisis,"llamada2")
 
 
 def parametrosReales2():
 	if(verbose):
 		print("parametrosReales2")
 	if ( preanalisis == "coma"):
-			 match("coma")
-			 parametrosReales()
-			 parametrosReales2()
+		match("coma")
+		parametrosReales()
+		parametrosReales2()
 
 #Procedimiento Match dado en la teoria
 def match(t):
@@ -569,8 +571,8 @@ def match(t):
 	global preanalisis
 	
 	if(preanalisis == t):
-                if(verbose):
-                        print(">Match:"+preanalisis)        
+		if(verbose):
+			print(">Match:"+preanalisis)        
 		posicion += 1
 		if(posicion < len(tokens)):
 			preanalisis = tokens[posicion]
@@ -582,7 +584,10 @@ def reportar(tipoError,simbolo,metodo):
 	exit(0)
 
 def reportarMatch(tipoError,simbolo,simboloanterior,metodo):
-	print "ERROR DETECTADO: "+ tipoError+ " "+ repr(preanalisis) +" despues de "+ repr(simboloanterior)+"\n"
+	if(simboloanterior == preanalisis):
+		print "ERROR DETECTADO: "+ tipoError+ " "+ repr(preanalisis) +". El archivo debe finalizar con END seguido de punto."+"\n"
+	else:
+		print "ERROR DETECTADO: "+ tipoError+ " "+ repr(preanalisis) +" despues de "+ repr(simboloanterior)+"\n"
 	exit(0)
 
 	
