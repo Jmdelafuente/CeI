4fc7fde9baacbf4cead34762786ca1321bb59ffe
dbonet <dbonet@pxe-server>
2018-09-06 18:01:48 -0300

    inicios de tabla de simbolos

---
+++ Pruebas/tablaSimbolos.py
@@ -0,0 +1,14 @@
+
+
+#se agrega un nuevo ambiente
+list.append(x)
+
+
+#se termina de impeccionar el ambiente actual y se elimina de la lista general
+list.remove(x)
+
+
+def ambiente():
+	#if(verbose):
+		#print("ambiente")
+	
