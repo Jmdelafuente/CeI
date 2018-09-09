901a7831bd841a402e07c8dfce81c525b06e0e25
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2018-09-09 18:44:29 -0300

    Mejoras en la documentación de la tabla de simbolos

--- Testing/tabla.py
+++ Testing/tabla.py
@@ -1,13 +1,13 @@
 # coding=utf-8
-'Nested contexts trees for implementing nested scopes (static or dynamic)'
+'Arboles de contextos anidados para implementar alcances anidados (estaticos o dinamicos)'
 
 from collections import MutableMapping
 from itertools import chain, imap
 
 class Tabla(MutableMapping):
-    ''' Nested contexts -- a chain of mapping objects.
+    ''' Tabla que permite Ambientes anidados -- una cadena de objetos.
 
-    c = Tabla()           Crear tabla root
+    c = Tabla()             Crear tabla root
     d = c.new_child()       Crear tabla hijo anidado. Inherente a enable_nonlocal
     e = c.new_child()       Hijo de c, independiente de d
     e.root                  Root -- exactamente como los globals de Python
@@ -22,32 +22,21 @@ class Tabla(MutableMapping):
     len(d)                  Cantidad de valores anidados
     d.items()               TODOS los valores anidados
 
-    Mutations (such as sets and deletes) are restricted to the current context
-    when "enable_nonlocal" is set to False (the default).  So c[k]=v will always
-    write to self.map, the current context.
-    
     Las modificaciones (como sets y deletes) están restringidas a la tabla actual 
     cuando "enable_nonlocal" está establecido en False (valor predeterminado). 
     Entonces c[k]=v siempre escribe en self.map, en la tabla actual.
 
-    But with "enable_nonlocal" set to True, variable in the enclosing contexts
-    can be mutated.  For example, to implement writeable scopes for nonlocals:
-	
-	Pero con "enable_nonlocal" establecido en True, la variable en la clausura de la tabla
+    Pero con "enable_nonlocal" establecido en True, la variable en la clausura de la tabla
 	se pueden modificar. Por ejemplo, para implementar ambientes editables para nonlocals:
 	
         nonlocals = c.parent.new_child(enable_nonlocal=True)
         nonlocals['y'] = 10     #sobrescribir la entrada existente en un ambiente anidado # overwrite existing entry in a nested scope
 
 	Para emular los valores globals() de Python, leer y modificar desde la tabla raíz:
-    To emulate Python's globals(), read and write from the the root context:
-
+    
         globals = c.root        #buscar la tabla más externa #look-up the outermost enclosing context
         globals['x'] = 10       # asignar directamente a ese tabla #assign directly to that context
 
-    To implement dynamic scoping (where functions can read their caller's
-    namespace), pass child contexts as an argument in a function call:
-    
     Para implementar el alcance dinámico (donde las funciones pueden leer el 
     espacio de nombres de su intermediario), las tablas secundarias se pasan como un 
     argumento en una llamada a función:
@@ -62,7 +51,7 @@ class Tabla(MutableMapping):
 
     '''
     def __init__(self, enable_nonlocal=False, parent=None):
-        'Create a new root context'
+        'Crear nuevo contexto Raiz'
         self.parent = parent
         self.enable_nonlocal = enable_nonlocal
         self.map = {}
@@ -71,13 +60,13 @@ class Tabla(MutableMapping):
             self.maps += parent.maps
 
     def new_child(self, enable_nonlocal=None):
-        'Make a child context, inheriting enable_nonlocal unless specified'
+        'Generar nuevo hijo, hereda el valor de enable_nonlocal por defecto'
         enable_nonlocal = self.enable_nonlocal if enable_nonlocal is None else enable_nonlocal
         return self.__class__(enable_nonlocal=enable_nonlocal, parent=self)
 
     @property
     def root(self):
-        'Return root context (highest level ancestor)'
+        'Getter para el ambente raiz (el ancestro mas externo)'
         return self if self.parent is None else self.parent.root
 
     def __getitem__(self, key):
