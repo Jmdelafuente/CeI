59b87dc7115ce3c24242b4f5da53e02fffc91682
jmdelafuente <jmdelafuente@pxe-server>
2018-09-20 16:30:58 -0300

    Nuevo ejemplo

---
+++ Testing/pascalCompleto2.pas
@@ -0,0 +1,55 @@
+PROGRAM prueba;
+var
+{Comentario seguramente util y perspicaz}
+a,b : integer;
+c : boolean;
+d : boolean;
+
+procedure funcion;
+var
+a : integer;
+begin
+	a := 10<9;
+end;
+
+function p1(zz:integer):boolean;
+var
+a : integer;
+begin
+end;
+
+procedure pa1(yy:boolean;xx:integer);
+var
+i,ii : integer;
+begin
+write(i+ii);
+end;
+
+procedure pa2;
+var
+i,ii:boolean;
+begin
+i:=i+ii;
+end;
+
+function pa3(ww:integer;www:integer;u:integer):boolean;
+var
+t:integer;
+t,u,v:boolean;
+
+begin
+	a := 10<9;
+	p1 := true;
+end;
+
+BEGIN
+b:=3+b>6 or (2<>c and c<4);
+d:=b and false;
+if not(a > b) THEN
+	c := true or false
+else
+	e:=false;
+noidentado;
+
+
+END.
